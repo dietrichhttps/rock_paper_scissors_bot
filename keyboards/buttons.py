42c6714ed646ee5a32f77ac7e5c8138ab6ec8667
Alice Glass <abayhanov1998@gmail.com>
2024-02-18 12:49:50 +0300

    Добавил пояснения к коду и укоротил длину строк для читаемости

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -2,9 +2,16 @@ from aiogram.types import KeyboardButton
 
 from lexicon.lexicon import LEXICON_RU
 
-yes_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['start']['yes'])
-no_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['start']['no'])
+# Кнопки да и нет
+yes_btn: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['button_texts']['start']['yes'])
+no_btn: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['button_texts']['start']['no'])
 
-rock_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['rock'])
-paper_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['paper'])
-scissors_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['scissors'])
+# Кнопки камень ножницы бумага
+rock_btn: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['button_texts']['figures']['rock'])
+paper_btn: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['button_texts']['figures']['paper'])
+scissors_btn: KeyboardButton = KeyboardButton(
+    text=LEXICON_RU['button_texts']['figures']['scissors'])
