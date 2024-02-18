368b647cc4824d36a06443b10d7a6056e82480ab
Alice Glass <abayhanov1998@gmail.com>
2024-02-18 12:47:41 +0300

    Добавил аннотации типов

--- keyboards/buttons.py
+++ keyboards/buttons.py
@@ -2,9 +2,9 @@ from aiogram.types import KeyboardButton
 
 from lexicon.lexicon import LEXICON_RU
 
-yes_btn = KeyboardButton(text=LEXICON_RU['button_texts']['start']['yes'])
-no_btn = KeyboardButton(text=LEXICON_RU['button_texts']['start']['no'])
+yes_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['start']['yes'])
+no_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['start']['no'])
 
-rock_btn = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['rock'])
-paper_btn = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['paper'])
-scissors_btn = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['scissors'])
+rock_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['rock'])
+paper_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['paper'])
+scissors_btn: KeyboardButton = KeyboardButton(text=LEXICON_RU['button_texts']['figures']['scissors'])
